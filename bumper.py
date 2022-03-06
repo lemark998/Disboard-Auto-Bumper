fb97a96c7002add377f0a0acb406196d3add6304
shsagnik <42864237+shsagnik@users.noreply.github.com>
2022-03-06 13:52:16 +0530

    Update bumper.py

--- bumper.py
+++ bumper.py
@@ -1,12 +1,8 @@
-import discord, os, time
+import discord,discum, os, time     
+bot = discum.Client(token=os.getenv("TOKEN"), log=False)
+from discum.utils.slash import SlashCommander
 from discord.ext import commands
 
-
-
-# Uncomment the line below if you are wanting to host this on heroku and are using an environment variable to store the token.
-token = os.getenv("TOKEN")
-# If you are using this on a server or your home pc uncomment the line below and put the discord token for the account you want it to auto bump on.
-#token = "TOKEN"
 
 bot = commands.Bot(command_prefix = "--", self_bot=True)
 
@@ -20,9 +16,29 @@ async def on_ready():
 @bot.command()
 async def bla(ctx):
     while True:
-        await ctx.send("/bump ")
+        def slashCommandTest(resp, guildID, channelID, botID):
+	        if resp.event.ready_supplemental:
+		        bot.gateway.request.searchSlashCommands(guildID, limit=10, query="bump") #query slash cmds
+	        if resp.event.guild_application_commands_updated:
+		        bot.gateway.removeCommand(slashCommandTest) #because 2 guild_app_cmd_update events are received...idk ask discord why
+		        slashCmds = resp.parsed.auto()['application_commands'] #get the slash cmds
+		        s = SlashCommander(slashCmds, application_id=botID) #for easy slash cmd data creation
+		        data = s.get(['bump',' '], inputs={'name':'test'})
+	        	bot.triggerSlashCommand(botID, channelID=channelID, guildID=guildID, data=data, sessionID=bot.gateway.session_id) #and send it off
+	        	bot.gateway.close() #optional. It's better to remove this line actually.
+
+        guildID = "856560014311292958"
+        channelID = "858061757775872011"
+        botID = "302050872383242240"
+        bot.gateway.command(
+	        {
+		        "function": slashCommandTest,
+		        "params": {"guildID": guildID, "channelID": channelID, "botID": botID},
+	        }
+        )
+        bot.gateway.run()
         time.sleep(8125)
 
-
+        
 
 bot.run(token, bot = False)
