6c4def15401ce66096255d832dd814b5c5ed0ddb
Lion <72986797+lemark998@users.noreply.github.com>
2022-09-11 20:32:22 -0500

    Update bumper.py

--- bumper.py
+++ bumper.py
@@ -23,8 +23,8 @@ while x:
             print(mes)
             bot.gateway.close() #optional. It's better to remove this line actually.
 
-    guildID = "1016368074691199018"
-    channelID = "1016733778569330722"
+    guildID = "1008574929022763101"
+    channelID = "1010405489793826836"
     botID = "302050872383242240"
     bot.gateway.command(
         {
@@ -33,7 +33,7 @@ while x:
         }
     )
     bot.gateway.run()
-    time.sleep(random.randint(8200, 8300))
+    time.sleep(random.randint(7300, 7400))
 
 
 
